2ba56aea9b40257ea02070e9aeabdf6865edae4b
venkat21032005 <venkatchowdary2103@gmail.com>
2025-08-13 20:09:15 +0530

    Fix: Update CREATE_CHAT mutation to include user_id

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -2,12 +2,13 @@ import { gql } from '@apollo/client';
 
 // Chat Mutations
 export const CREATE_CHAT = gql`
-  mutation CreateChat($title: String!) {
-    insert_chats_one(object: { title: $title }) {
+  mutation CreateChat($title: String!, $user_id: uuid!) {
+    insert_chats_one(object: { title: $title, user_id: $user_id }) {
       id
       title
       created_at
       updated_at
+      user_id
     }
   }
 `;
